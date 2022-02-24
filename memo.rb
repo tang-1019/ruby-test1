5a74f2b86f0b60e6d64c8d68ceede4a9170f0f85
tang-1019 <mstg1019@gmail.com>
2022-02-24 22:36:40 +0900

    新規作成

---
+++ memo.rb
@@ -0,0 +1,30 @@
+require "csv"
+ 
+puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+
+memo_type = gets.chomp
+
+if memo_type == "1"
+    puts "拡張子を除いたファイル名を入力してください"
+    memo_tytle = gets.chomp
+    puts "メモしたい内容を入力してください"
+    puts "完了したら「Ｃｔｒｌ + Ｄ」を押してください"
+    CSV.open("#{memo_tytle}.csv","w") do |memo|
+        content = readlines
+        memo << content
+    end
+
+elsif memo_type == "2"
+    puts "拡張子を除いたファイル名を入力してください"
+    memo_tytle = gets.chomp
+    puts "メモしたい内容を入力してください"
+    puts "完了したら「Ｃｔｒｌ + Ｄ」を押してください"
+    CSV.open("#{memo_tytle}.csv","a") do |memo|
+        content = readlines
+        memo << content
+    end
+
+else
+    puts "1,2以外が入力されたので処理を終了します"
+    
+end
